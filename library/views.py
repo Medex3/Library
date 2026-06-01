a367f61f9839853000c34f9cdcea6eaccc2ab0d9
Medex3 <medex333@mail.ru>
2026-06-01 20:04:26 +0300

    Представления

--- library/views.py
+++ library/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from .models import Book
 
-# Create your views here.
+
+def home(request):
+    recent_books = Book.objects.all().order_by('-created_at')[:5]
+    return render(request, 'library/home.html', {'recent_books': recent_books})
+
+
+def catalog(request):
+    books = Book.objects.all()
+    return render(request, 'library/catalog.html', {'books': books})
+
+
+def about(request):
+    return render(request, 'library/about.html')
+
+
+def contacts(request):
+    return render(request, 'library/contacts.html')
