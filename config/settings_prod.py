080707cd60fc78cf5d1147c185ebb267ed10f674
Medex3 <medex333@mail.ru>
2026-06-06 12:08:02 +0300

    Развертывание

--- config/settings_prod.py
+++ config/settings_prod.py
@@ -2,24 +2,17 @@ from .settings import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['medex1447.pythonanywhere.com']
+ALLOWED_HOSTS = ['Medex1447.pythonanywhere.com']
 
-# Статика
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_URL = '/static/'
 
-# Медиа
 MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
-
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': '/home/medex1447/library.db',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
-SECRET_KEY = 'django-insecure-)-*qtw3i@qulajkl04oni!k)uwer)p(-3##9_2rb5#!-g5=ys4'
-CSRF_COOKIE_SECURE = True
-SESSION_COOKIE_SECURE = True
